a76529a02ac462de416823b8b99c1db904dbe1cd
Andrius-Dobrotinas <andrius.dobrotinas@gmail.com>
2016-12-19 22:33:01 +0200

    Added releases and more things to import.sql. Made ReleaseRepositoryDefault.getAllReleases select actual items from the database.

--- src/main/java/com/andrewd/recordlabel/data/repository/ReleaseRepositoryDefault.java
+++ src/main/java/com/andrewd/recordlabel/data/repository/ReleaseRepositoryDefault.java
@@ -38,12 +38,7 @@ public class ReleaseRepositoryDefault implements ReleaseRepository {
 
     // TODO: add the whole batching mechanism
     public List<Release> getAllReleases() {
-        // TODO: replace with real thing
-        List<Release> result = new ArrayList<>();
-        for(int i = 0; i < 3; i++) {
-            result.add(getReleaseStub(i));
-        }
-        return result;
+        return em.createQuery("select i from Release", Release.class).getResultList();
     }
 
     public int getTotalReleaseCount() {
@@ -66,53 +61,5 @@ public class ReleaseRepositoryDefault implements ReleaseRepository {
      */
     public List<Artist> getAllArtists() {
         return em.createQuery("select i from Artist i", Artist.class).getResultList();
-    }
-
-
-    private Release getReleaseStub(int id) {
-        Release r = new Release();
-        r.id = id;
-        r.artist = getArtistStub();
-        r.catalogueNumber = "CK32000";
-        r.date = 1973;
-        r.length = 43;
-        r.media = getMediaStub();
-        r.printStatus = PrintStatus.OutOfPrint;
-        //r.text = "One of THE greatest records on earth!";
-        r.title = "Raw Power";
-        r.tracks = getTracksStub();
-
-        return r;
-    }
-
-    private Artist getArtistStub() {
-        Artist a = new Artist();
-        a.name = "Iggy & The Stooges";
-        a.text = "Greatest band on earth!";
-        return a;
-    }
-    private MediaType getMediaStub() {
-        MediaType m = new MediaType();
-        m.id = 1;
-        m.text = "LP";
-        return m;
-    }
-
-    private java.util.ArrayList<Track> getTracksStub() {
-        Track t1 = new Track();
-        t1.id = 1;
-        t1.title = "Search And Destroy";
-        t1.reference = "https://www.youtube.com/v/BJIqnXTqg8I";
-
-        Track t2 = new Track();
-        t2.id = 2;
-        t2.title = "Gimme Danger";
-        t2.reference = "https://www.youtube.com/v/JFAcOnhcpGA";
-
-        java.util.ArrayList<Track> tracks = new java.util.ArrayList<Track>();
-        tracks.add(t1);
-        tracks.add(t2);
-
-        return tracks;
     }
 }
